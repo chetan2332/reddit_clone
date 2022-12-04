289698ae9cd55f3f6c2a9c5f8ef08ae6e6910a2b
chetan2332 <101946181+chetan2332@users.noreply.github.com>
2022-12-04 15:57:56 +0530

    drawer added and list of user communities visible

--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/loader.dart';
+import 'package:reddit_clone/features/community/controller/community_controller.dart';
 
 class CreateCommunityScreen extends ConsumerStatefulWidget {
   const CreateCommunityScreen({super.key});
@@ -18,47 +20,58 @@ class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
     super.dispose();
   }
 
+  void createCommunity() {
+    ref
+        .read(communityControllerProvider.notifier)
+        .createCommunity(communityNameController.text.trim(), context);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(communityControllerProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('Community name')),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          const Text('Community Name'),
-          const SizedBox(height: 10),
-          TextField(
-            controller: communityNameController,
-            decoration: const InputDecoration(
-              hintText: 'r/Community_name',
-              contentPadding: EdgeInsets.all(18),
-              filled: true,
-              border: InputBorder.none,
+      body: isLoading
+          ? const Loader()
+          : Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Community Name'),
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: communityNameController,
+                      decoration: const InputDecoration(
+                        hintText: 'r/Community_name',
+                        contentPadding: EdgeInsets.all(18),
+                        filled: true,
+                        border: InputBorder.none,
+                      ),
+                      maxLength: 21,
+                    ),
+                    const SizedBox(height: 30),
+                    Align(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        onPressed: createCommunity,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(18.0),
+                          child: Text(
+                            'Create Community',
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ),
+                      ),
+                    )
+                  ]),
             ),
-            maxLength: 21,
-          ),
-          const SizedBox(height: 30),
-          Align(
-            alignment: Alignment.center,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Text(
-                  'Create Community',
-                  style: TextStyle(fontSize: 17),
-                ),
-              ),
-            ),
-          )
-        ]),
-      ),
     );
   }
 }
